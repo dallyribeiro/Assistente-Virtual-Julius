e486d04c105fb0bf88babee520949a6b5f1ed957
Kauã Borgarelli Tavares <79945984+Borgarelli@users.noreply.github.com>
2021-03-23 20:26:08 -0300

    Add files via upload

---
+++ Assistente Virtual/Cotação_Dia.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+requisição = requests.get('https://economia.awesomeapi.com.br/all')
+
+cotação = requisição.json()
+
+Valor_moeda = input('Qual o valor da moeda?: ')
+
+if Valor_moeda == ('Dolar'):
+    print('#### Cotação do Dolar ####')
+
+    print('Moeda: ' + cotação ['USD'] ['name'])
+    print('Data: ' + cotação ['USD'] ['create_date'])
+    print('Valor Atual: R$: ' + cotação ['USD']['bid'])
+
+elif Valor_moeda == ('Euro'):
+    print('#### Cotação do Euro ####')
+
+    print('Moeda: ' + cotação['EUR']['name'])
+    print('Data: ' + cotação['EUR']['create_date'])
+    print('Valor Atual: R$: ' + cotação['EUR']['bid'])
+
+elif Valor_moeda == ('Libra'):
+    print('#### Cotação do Libra ####')
+
+    print('Moeda: ' + cotação ['GBP'] ['name'])
+    print('Data: ' + cotação ['GBP'] ['create_date'])
+    print('Valor Atual: R$: ' + cotação['GBP'] ['bid'])
+
+elif Valor_moeda == ('Bitcoin'):
+    print('#### Cotação do Bitcoin ####')
+    print('Moeda: ' + cotação ['BTC'] ['name'])
+    print('Data: ' + cotação ['BTC'] ['create_date'])
+    print('Valor Atual: R$: ' + cotação ['BTC'] ['bid'])
+
+
+
