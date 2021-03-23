26ec1201b89aa2620dc72890558fb34f49410e79
Kauã Borgarelli Tavares <79945984+Borgarelli@users.noreply.github.com>
2021-03-23 20:33:12 -0300

    Update Cotação_Dia.py

--- Assistente Virtual/Cotação_Dia.py
+++ Assistente Virtual/Cotação_Dia.py
@@ -12,27 +12,28 @@ if Valor_moeda == ('Dolar'):
 
     print('Moeda: ' + cotação ['USD'] ['name'])
     print('Data: ' + cotação ['USD'] ['create_date'])
-    print('Valor Atual: R$: ' + cotação ['USD']['bid'])
+    print('Valor Atual R$: ' + cotação ['USD']['bid'])
 
 elif Valor_moeda == ('Euro'):
     print('#### Cotação do Euro ####')
 
-    print('Moeda: ' + cotação['EUR']['name'])
-    print('Data: ' + cotação['EUR']['create_date'])
-    print('Valor Atual: R$: ' + cotação['EUR']['bid'])
+    print('Moeda: ' + cotação ['EUR'] ['name'])
+    print('Data: ' + cotação ['EUR'] ['create_date'])
+    print('Valor Atual R$: ' + cotação ['EUR'] ['bid'])
 
 elif Valor_moeda == ('Libra'):
     print('#### Cotação do Libra ####')
 
     print('Moeda: ' + cotação ['GBP'] ['name'])
     print('Data: ' + cotação ['GBP'] ['create_date'])
-    print('Valor Atual: R$: ' + cotação['GBP'] ['bid'])
+    print('Valor Atual R$: ' + cotação['GBP'] ['bid'])
 
 elif Valor_moeda == ('Bitcoin'):
     print('#### Cotação do Bitcoin ####')
+    
     print('Moeda: ' + cotação ['BTC'] ['name'])
     print('Data: ' + cotação ['BTC'] ['create_date'])
-    print('Valor Atual: R$: ' + cotação ['BTC'] ['bid'])
+    print('Valor Atual R$: ' + cotação ['BTC'] ['bid'])
 
 
 
