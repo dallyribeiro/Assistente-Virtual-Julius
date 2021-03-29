f356102a6f692037858332e24e52e9f3691ce1c4
taniacruzz <79020769+taniacruzz@users.noreply.github.com>
2021-03-28 23:12:05 -0300

    Add files via upload
    Metas

---
+++ main.py
@@ -0,0 +1,25 @@
+# Meta
+
+N1 = int(input('Quanto deseja investir?'))
+N2 =  int(input('Quanto deseja alcançar?'))
+
+def inicio_meta(a, b,):
+
+   Vf= b /a
+   print( 'Você tem {} , deseja ter {} , vai atigir sua meta em {} meses.' .format(a, b, Vf))
+
+
+
+r = inicio_meta(N1, N2,)
+
+
+
+
+
+
+
+
+
+
+
+
