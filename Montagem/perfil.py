28ad4572001485373357656c99da5d89f6ab6ca0
taniacruzz <79020769+taniacruzz@users.noreply.github.com>
2021-04-18 18:11:09 -0300

    Update perfil.py

--- Montagem/perfil.py
+++ Montagem/perfil.py
@@ -1,24 +1,25 @@
 from config import sai_som
 
-def perfil(entrada):
+def perfil():
     result = 0
-    p1 = str(input('Você já fez sua reserva de emergência? ')).upper()
     sai_som('Você já fez sua reserva de emergência? ')
+    p1 = str(input('Você já fez sua reserva de emergência? ')).upper()
 
-    p2 = str(input('Aceitaria riscos para maximizar ganhos? ')).upper()
     sai_som('Aceitaria riscos para maximizar ganhos? ')
+    p2 = str(input('Aceitaria riscos para maximizar ganhos? ')).upper()
 
-    p3 = str(input('Tem a meta de guardar dinheiro para um futuro a longo prazo, como aposentadoria? ')).upper()
     sai_som('Tem a meta de guardar dinheiro para um futuro a longo prazo, como aposentadoria? ')
+    p3 = str(input('Tem a meta de guardar dinheiro para um futuro a longo prazo, como aposentadoria? ')).upper()
 
-    p4 = str(input('Você tem experiência ou formação no mercado financeiro? ')).upper()
     sai_som('Você tem experiência ou formação no mercado financeiro? ')
+    p4 = str(input('Você tem experiência ou formação no mercado financeiro? ')).upper()
 
-    p5 = str(input('Você já investe ou investiu nos últimos meses? ')).upper()
     sai_som('Você já investe ou investiu nos últimos meses? ')
+    p5 = str(input('Você já investe ou investiu nos últimos meses? ')).upper()
 
-    p6 = str(input('Você conseguiria destinar de 10% a 15% do seu salario a investimentos? ')).upper()
     sai_som('Você conseguiria destinar de 10% a 15% do seu salario a investimentos? ')
+    p6 = str(input('Você conseguiria destinar de 10% a 15% do seu salario a investimentos? ')).upper()
+
 
     if p1 == 'SIM':
         result +=2
@@ -55,6 +56,9 @@ def perfil(entrada):
     if result >= 10:
         print(f'Você é um investidor EXPERIENTE pois sua pontuação foi de {result} pontos!')
         sai_som(f'Você é um investidor Experiente pois sua pontuação foi de {result} pontos!')
+
+
+   
 
 
    
