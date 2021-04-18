ec220db90d17014a065837aa3518d014b797a8ac
taniacruzz <79020769+taniacruzz@users.noreply.github.com>
2021-04-18 17:41:48 -0300

    Update codigo.py

--- Montagem/codigo.py
+++ Montagem/codigo.py
@@ -1,18 +1,20 @@
 #Pergunta para o cliente
-cod_acao = str(input('De qual empresa deseja saber o código de ação? '))  
 
-#Imprimi o valor e printa o código de ação
-if cod_acao == ('Netflix'):
-    print('NFLX34') 
-    
-elif cod_acao == ('Itaú'):
-    print('O código de ação dessa empresa é: ITUB3F')
+def codigo():
+    cod_acao = str(input('De qual empresa deseja saber o código de ação? '))
 
-elif cod_acao == ('Vale'):
-    print('O código de ação dessa empresa é: VALE5')
+    #Imprimi o valor e printa o código de ação
+    if cod_acao == ('Netflix'):
+        print('NFLX34')
 
-elif cod_acao == ('Petrobras'):
-    print('O código de ação dessa empresa é: PETR4F')
+    elif cod_acao == ('Itaú'):
+        print('O código de ação dessa empresa é: ITUB3F')
 
-elif cod_acao == ('Facebook'):
-    print('O código de ação dessa empresa é: FB')
+    elif cod_acao == ('Vale'):
+        print('O código de ação dessa empresa é: VALE5')
+
+    elif cod_acao == ('Petrobras'):
+        print('O código de ação dessa empresa é: PETR4F')
+
+    elif cod_acao == ('Facebook'):
+        print('O código de ação dessa empresa é: FB')
